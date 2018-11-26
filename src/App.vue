bf01432da8102ca0fdadbbb50da0e9658d81ed1e
rahulspace <rahultp518@gmail.com>
2018-11-26 22:14:18 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body, h1, h2, h3, h4, h5, h6, p {
+  margin: 0;
+}
+#app {
+
+}
+</style>
