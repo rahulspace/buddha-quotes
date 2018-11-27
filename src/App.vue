0455f5372815331a8f2045fbf9891f2a9adbf207
rahulspace <rahultp518@gmail.com>
2018-11-27 22:42:32 +0530

    Import pt sans fonts

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,21 @@ export default {
 <style>
 body, h1, h2, h3, h4, h5, h6, p {
   margin: 0;
+  font-family: pt-sans;
 }
+
+@font-face {
+  font-family: pt-sans;
+  font-weight: 400;
+  src: url('./assets/fonts/pt-regular.woff');
+}
+
+@font-face {
+  font-family: pt-sans;
+  font-weight: 700;
+  src: url('./assets/fonts/pt-bold.woff');
+}
+
 #app {
 
 }
